aca9d1f6bbcfb7c9c9bf2c573e08d651e0d39e25
Rodrigo Araujo <rasch.dev@gmail.com>
2025-08-31 07:56:28 -0300

    Implemented base Pagina

--- src/questao1/Pagina.java
+++ src/questao1/Pagina.java
@@ -1,5 +1,26 @@
 package questao1;
 
 public class Pagina {
+	private String titulo, url;
+	
+	public Pagina(String titulo) {
+		this.titulo = titulo;
+		this.url = "/" + titulo.replaceAll("\\s", "").toLowerCase();
+	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Título: %s. URL: %s.", titulo, url);
+	}	
 }
