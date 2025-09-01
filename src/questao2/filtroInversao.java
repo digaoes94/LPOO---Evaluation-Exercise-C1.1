5d3fa198b6a90da7cd3d898b2c88ce3c8df26096
Rodrigo Araujo <rasch.dev@gmail.com>
2025-09-01 08:00:56 -0300

    Questao2 completed

--- src/questao2/filtroInversao.java
+++ src/questao2/filtroInversao.java
@@ -2,11 +2,61 @@
 
 package questao2;
 
+import java.util.LinkedList;
+import java.util.TreeSet;
+
 public class filtroInversao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LinkedList<Integer> lista1 = new LinkedList<Integer>();
+		LinkedList<Integer> lista2;
+		LinkedList<Integer> listaAux = new LinkedList<Integer>();
+		TreeSet<Integer> arv;
+		int aux;
+		
+		// PREENCHER LISTA1 ---------------------------------------------------------------------------------------------------
+		for(int a = 0; a < 10; a++) {
+			aux = (int) (Math.random() * 20 + 1);
+			lista1.push(aux);
+		}
+		//---------------------------------------------------------------------------------------------------------------------
+		System.out.println("Lista 1:   " + lista1.toString());
+		
+		// FILTRAR LISTA1 -----------------------------------------------------------------------------------------------------
+		for(Integer i : lista1) {
+			if(listaAux.isEmpty()) {
+				listaAux.push(i);
+			}
+			else if(listaAux.indexOf(i) == -1) {
+				// addLast para ver melhor a diferença nas listas após System.out
+				listaAux.addLast(i);
+			}
+		}
+		//---------------------------------------------------------------------------------------------------------------------
+		System.out.println("Lista Aux: " + listaAux.toString());
+		
+		// ORDENAR LISTA2 -----------------------------------------------------------------------------------------------------
+		arv = new TreeSet<Integer>(listaAux);
+		lista2 = new LinkedList<Integer>(arv);
+		arv = null;
+		//---------------------------------------------------------------------------------------------------------------------
+		System.out.println("Lista 2:   " + lista2.toString()); // lista2 já filtrada e ordenada
+		
+		// INVERTER ORDEM DA SEGUNDA METADE DA LISTA2 -------------------------------------------------------------------------
+		aux = lista2.size() / 2;
+		listaAux.clear();
+		
+		for(int a = 0; a < aux; a++) {
+			listaAux.add(lista2.removeLast());
+		}
+		
+		for(int a = 0; a < aux; a++) {
+			lista2.addLast(listaAux.removeFirst());
+		}
+		//--------------------------------------------------------------------------------------------------------------------
+		
+		System.out.println("Lista Final");
+		System.out.println("Lista 2:   " + lista2.toString());
 	}
 
 }
